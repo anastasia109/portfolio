3fa169afb08eeb188eaa9e7aa3de4c3f8889cb50
Anastasiia Drobotun <anastasiia.drobotun@skynix.co>
2022-06-25 20:04:47 +0300

    bug fix

--- src/components/social/social.js
+++ src/components/social/social.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPhone, faEnvelope} from '@fortawesome/fontawesome-free-solid'
-import {faFacebookF, faLinkedinIn, faTelegram} from '@fortawesome/free-brands-svg-icons'
+import {faFacebookF, faLinkedinIn} from '@fortawesome/free-brands-svg-icons'
 
 import './social.css';
 
